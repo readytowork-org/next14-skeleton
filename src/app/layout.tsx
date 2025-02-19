458f73202dc3766d2c7dee06fe382f5a83385021
rajchaudhary <craj68168@gmail.com>
2025-02-19 14:02:13 +0545

    feat:layout refactor
    (cherry picked from commit 9dc18d460188c7d3b4d6089187dd0cf66abb8cd1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,12 @@
-import type { Metadata, Viewport } from "next";
+import type { Viewport } from "next";
 import { HIRAGINO_SANS } from "@/src/fonts";
 import "./global.css";
 import QueryClientProviders from "../components/organisms/QueryClientProviders";
 import { NextIntlClientProvider } from "next-intl";
-import { getLocale, getMessages, getTranslations } from "next-intl/server";
-import { BackToTop, GlobalTagManager } from "../components/atoms";
+import { getLocale, getMessages } from "next-intl/server";
+import { BackToTop } from "../components/atoms";
 import { ProgressBarProvider } from "../components/atoms/ProgressBarProvider";
 import { LanguageSwitcher } from "../components/atoms/LanguageSwitch";
-
-export async function generateMetadata(): Promise<Metadata> {
-  const t = await getTranslations("Metadata.global");
-  const imageUrl = process.env.NEXT_PUBLIC_BASEURL + "/assets/default-og.png";
-
-  const isProduction =
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ||
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "Production";
-
-  return {
-    applicationName: "みんなのドライバ",
-    title: t("title"),
-    description: t("description"),
-    twitter: {
-      title: t("title"),
-      description: t("description"),
-      images: [
-        {
-          url: imageUrl,
-          alt: t("siteName"),
-        },
-      ],
-      card: "summary_large_image",
-    },
-    openGraph: {
-      siteName: t("siteName"),
-      title: t("title"),
-      description: t("description"),
-      images: [
-        {
-          url: imageUrl,
-          alt: t("siteName"),
-        },
-      ],
-    },
-    robots: {
-      index: isProduction,
-      follow: isProduction,
-    },
-  };
-}
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -65,11 +24,6 @@ export default async function RootLayout({
 
   return (
     <html lang={"en"}>
-      <head>
-        <link rel={"preload"} href={"/icons/banner.webp"} as={"image"} />
-        <GlobalTagManager />
-        <link rel={"preload"} as={"image"} href={"/icons/banner.webp"} />
-      </head>
       <body className={`${HIRAGINO_SANS.variable}`}>
         <NextIntlClientProvider messages={messages} locale={locale}>
           <ProgressBarProvider>
